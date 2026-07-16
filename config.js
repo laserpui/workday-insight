0999ed5dd323bee00b4d18c3ba5f99f752e7096c
laserpui <nattachai_p@hotmail.com>
2026-07-16 13:02:03 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  * หลัง Deploy เวอร์ชันใหม่ ให้นำ URL ที่ลงท้ายด้วย /exec มาใส่ที่นี่
  */
 window.WORKDAY_CONFIG = {
-  appUrl: 'https://script.google.com/macros/s/AKfycbwTYANs_Weqv7_GPAqDS4bi-F-DXTlW1IZu8SG5DraqFxCUhgsoSRY_7bm8F143TKwm/exec'
+  appUrl: 'https://script.google.com/macros/s/AKfycbxQIWZbFGu-fsy3jz4Xz49CN_9T8MJLzMU9hPQeJBlVwOsEwroLsh2hOanbHoiowihFSw/exec'
 };
