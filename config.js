aabbd17e59a726d34d7652c7ac9b26c63ec7eb64
laserpui <nattachai_p@hotmail.com>
2026-07-16 11:05:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * URL ของ Google Apps Script Web App
+ * หลัง Deploy เวอร์ชันใหม่ ให้นำ URL ที่ลงท้ายด้วย /exec มาใส่ที่นี่
+ */
+window.WORKDAY_CONFIG = {
+  appUrl: 'https://script.google.com/macros/s/AKfycbwTYANs_Weqv7_GPAqDS4bi-F-DXTlW1IZu8SG5DraqFxCUhgsoSRY_7bm8F143TKwm/exec'
+};
